aa1f9b0ca44f7b6619eecb7f7148130fa2edd023
ivan135 <1576475704@qq.com>
2018-07-28 17:33:33 +0800

    使用 chai.expect 添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -1,15 +1,99 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('cs-button', Button);
-Vue.component('cs-icon',Icon);
+Vue.component('cs-icon', Icon);
+Vue.component('cs-button-group', ButtonGroup);
 
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false,
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false,
     }
 });
+
+import chai from 'chai'
+
+const expect = chai.expect;
+//单元测试
+{
+    const Construc = Vue.extend(Button);
+    const vm = new Construc({
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-settings');
+}
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading');
+    vm.$el.remove();
+    vm.$destroy();
+}
+
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    vm.$el.remove();
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    vm.$el.remove();
+    vm.$destroy()
+}
+{
+    // mock
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    });
+    vm.$mount();
+    vm.$on('click', function () {
+        expect(1).to.eq(1)
+    });
+    // 希望这个函数被执行
+    let button = vm.$el;
+    button.click()
+}
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="cs-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+      export default {
+        mounted () {
+              for (let node of this.$el.children) {
+                    let name = node.nodeName.toLowerCase();
+                        if (name !== 'button') {
+                          console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
+                        }
+                 }
+            }
+      }
+</script>
+
+<style lang="scss">
+.cs-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    >.cs-button{
+        border-radius: 0;
+        &:not(:first-child){
+            margin-left:-1px;
+        }
+        &:first-child{
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+        }
+        &:last-child{
+            boder-top-right-radius:var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);;
+        }
+        &:hover{
+            position: relative;
+            z-index:1;
+        }
+    }
+}
+</style>
